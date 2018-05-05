75ce8e76dbbbe7029b28d356a461b8c9079a8199
Alnim <raytmilan@gmail.com>
2018-05-04 18:05:22 -0700

    Finished free, did it very inefficiently, but, adam will explain O(1) ;)

--- srcs/main.c
+++ srcs/main.c
@@ -20,6 +20,8 @@ void print_mlist(t_mlist *list_head)
 }
 void print_hippocampus()
 {
+
+	printf("-------- PRINTING HIPPOCAMPUS --------\n");
 	printf("Tiny:\n");
 	print_mlist(true_hippocampus->tiny_head);
 	printf("Small:\n");
@@ -108,6 +110,7 @@ void *allocate_inefficient(int mem_size, int size)
 		add_to_end_of_mlist(&(true_hippocampus->tiny_last), allocated_ptr, size,
 							STATUS_INUSE);
 		temp_last = true_hippocampus->tiny_last;
+		temp_last->page_start = temp_last;
 		add_to_end_of_mlist(&(true_hippocampus->tiny_last),
 							ptr_start_of_cache_left, length_of_cache_left,
 							STATUS_FREE);
@@ -118,6 +121,7 @@ void *allocate_inefficient(int mem_size, int size)
 		add_to_end_of_mlist(&(true_hippocampus->small_last), allocated_ptr,
 							size, STATUS_INUSE);
 		temp_last = true_hippocampus->small_last;
+		temp_last->page_start = temp_last;
 		add_to_end_of_mlist(&(true_hippocampus->small_last),
 							ptr_start_of_cache_left, length_of_cache_left,
 							STATUS_FREE);
@@ -128,6 +132,7 @@ void *allocate_inefficient(int mem_size, int size)
 		add_to_end_of_mlist(&(true_hippocampus->large_last), allocated_ptr,
 							size, STATUS_INUSE);
 		temp_last = true_hippocampus->large_last;
+		temp_last->page_start = temp_last;
 		add_to_end_of_mlist(&(true_hippocampus->large_last),
 							ptr_start_of_cache_left, length_of_cache_left,
 							STATUS_FREE);
@@ -269,6 +274,7 @@ void allocate_space_if_needed(void **ret, int mem_size, int size)
 }
 void *malloc(size_t size)
 {
+	printf("MALLOCING!\n");
 	int   mem_size;
 	void *ret;
 	if (!size)
@@ -280,19 +286,109 @@ void *malloc(size_t size)
 	return ret;
 }
 
-int main()
+t_mlist *find_mem_to_be_freed(void *ptr, t_mlist **page_start_elem,
+							  t_mlist **head_type_for_search)
 {
-	char hello = (char)malloc(16);
-	printf("%c", hello);
+	t_mlist *iter;
+	t_mlist *prev;
+
+	iter = true_hippocampus->tiny_head;
+	*head_type_for_search = iter;
+	(*page_start_elem) = iter;
+	while (iter)
+	{
+		if (iter->page_start != (*page_start_elem)->page_start)
+			(*page_start_elem) = prev;
+		if (iter->ptr_start == ptr)
+			return iter;
+		prev = iter;
+		iter = iter->next;
+	}
+	iter = true_hippocampus->small_head;
+	*head_type_for_search = iter;
+	(*page_start_elem) = iter;
+	while (iter)
+	{
+		if (iter->page_start != (*page_start_elem)->page_start)
+			(*page_start_elem) = prev;
+		if (iter->ptr_start == ptr)
+			return iter;
+		prev = iter;
+		iter = iter->next;
+	}
+	iter = true_hippocampus->large_head;
+	*head_type_for_search = iter;
+	(*page_start_elem) = iter;
+	while (iter)
+	{
+		if (iter->page_start != (*page_start_elem)->page_start)
+			(*page_start_elem) = prev;
+		if (iter->ptr_start == ptr)
+			return iter;
+		prev = iter;
+		iter = iter->next;
+	}
+	return NULL;
+}
+
+void if_unmap_possible_then_do(t_mlist *found_ptr,
+							   t_mlist *head_type_for_search,
+							   t_mlist *page_start_elem_prev)
+{
+	t_mlist *iter;
+	int		 size;
+
+	size = 0;
+	iter = found_ptr->page_start;
+	while (iter && iter->page_start == found_ptr->page_start)
+	{
+		if (iter->status == STATUS_INUSE)
+			return;
+		size += iter->length;
+		iter = iter->next;
+	}
+	if (page_start_elem_prev != head_type_for_search)
+		page_start_elem_prev->next = iter;
+	else
+	{
+		if(head_type_for_search == true_hippocampus->tiny_head)
+			true_hippocampus->tiny_head = iter;
+		if(head_type_for_search == true_hippocampus->small_head)
+			true_hippocampus->small_head = iter;
+		if(head_type_for_search == true_hippocampus->large_head)
+			true_hippocampus->large_head = iter;
+	}
+	printf("Head Type for search: %p | Size of UNMAP: %d\n", head_type_for_search, size);
+	printf("MEMUNMAPING");
+	munmap(found_ptr->page_start->ptr_start, size);
+}
+void free(void *ptr)
+{
+	t_mlist *found_ptr;
+	t_mlist *page_start_elem_for_munmap_search;
+	t_mlist *head_type_for_search;
+	printf("FREEING -> %p\n", ptr);
+	found_ptr = find_mem_to_be_freed(ptr, &page_start_elem_for_munmap_search,
+									 &head_type_for_search);
+	found_ptr->status = STATUS_FREE;
+	if_unmap_possible_then_do(found_ptr, head_type_for_search,
+							  page_start_elem_for_munmap_search);
+}
+int main(void)
+{
+	void *hello = malloc(18);
 	printf("Size of t_mlist: %d\n", (int)sizeof(t_mlist));
 	printf("Size of t_hippocampus: %d\n", (int)sizeof(t_hippocampus));
 	print_hippocampus();
-	printf("MALLOCING!\n");
 	int i = -1;
-	while (++i < 54)
+		free(hello);
+	while (++i < 2)
 	{
-		hello = (char)malloc(16);
+		hello = malloc(16);
+		print_hippocampus();
 		print_hippocampus();
 	}
+		free(hello);
+		print_hippocampus();
 	// printf("%c", hello);
 }
